b4f4c5aa2980d584911f18145528b315c427aeb9
AnastasiaGavrilova201 <81684665+AnastasiaGavrilova201@users.noreply.github.com>
2024-12-30 16:38:22 +0300

    добавила заголовок к графикам

--- service/frontend/pages/EDA.py
+++ service/frontend/pages/EDA.py
@@ -23,26 +23,27 @@ def graph(column):
     y.index = pd.date_range(start=min(data['dt_iso'])[:16], end=max(data['dt_iso'])[:16], freq="h").to_period()
     forecaster.fit(y)
     y_pred = forecaster.predict(fh=range(1, 25))
-    y_title = {'temp': u'\u00B0C', 'pressure': 'мм', 'wind_speed': 'м/с', 'humidity': '%'}
+    y_title = {'temp': '\u00B0C', 'pressure': 'мм', 'wind_speed': 'м/с', 'humidity': '%'}
+    gragh_title = {'temp': 'температуры', 'pressure': 'давления', 'wind_speed': 'скорости ветра', 'humidity': 'влажности'}
     fig1 = go.Figure()
-    fig1.add_trace(go.Scatter(x=y.index.to_timestamp(),
-                              y=y,
-                              mode='lines',
-                              name='Историчные данные'
+    fig1.add_trace(go.Scatter(x = y.index.to_timestamp(),
+                              y = y,
+                              mode = 'lines',
+                              name = 'Историчные данные'
                               )
                    )
-    fig1.add_trace(go.Scatter(x=y_pred.index.to_timestamp(),
-                              y=y_pred,
-                              mode='lines',
-                              name='Прогноз'
+    fig1.add_trace(go.Scatter(x = y_pred.index.to_timestamp(),
+                              y = y_pred,
+                              mode = 'lines',
+                              name = 'Прогноз'
                               )
                    )
 
-    fig1.update_layout(#title='Прогноз температуры',
-                       xaxis_title='Дата',
-                       yaxis_title=y_title[column],
-                       height=500,
-                       width=1000
+    fig1.update_layout(title = f'Динамика {gragh_title[column]}',
+                       xaxis_title = 'Дата и время',
+                       yaxis_title = y_title[column],
+                       height = 500,
+                       width = 1000
                        )
 
     st.plotly_chart(fig1)
